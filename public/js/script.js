422218ab84cb15efc2c4448f5bde09f716d8b97e
Julia <juhcarvalho@gmail.com>
2021-09-16 20:01:04 -0300

    commit 3 calculadora de notas

--- public/js/script.js
+++ public/js/script.js
@@ -3,12 +3,14 @@ function calculoMedia() {
     let nota2 = parseInt(document.getElementById('nota2').value);
     let mediaNotas = (nota1 + nota2) / 2
     if (mediaNotas >= 7) {
-        window.alert("Você foi aprovado com média " + mediaNotas +"!");
+        let media1 = document.getElementById("textomedia");
+        let textomedia = "Parabéns! Você foi aprovado com média " + mediaNotas + ".";
+        media1.innerHTML = textomedia;
     } else {
-        window.alert("Você foi reprovado com média " + mediaNotas +"!");
+        let media2 = document.getElementById("textomedia");
+        let textomedia = "Infelizmente você foi reprovado com média " + mediaNotas + ".";
+        media2.innerHTML = textomedia;
     }
-    document.getElementById("nota1").value = "";
-    document.getElementById("nota2").value = "";
 }
 
 

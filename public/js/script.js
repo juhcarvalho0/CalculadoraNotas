3d6a535e424c4707d3fe68966f7bdee220fde176
Julia <juhcarvalho@gmail.com>
2021-09-16 21:33:48 -0300

    commit 5 calculadora de notas

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 function calculoMedia() {
-    let nota1 = parseInt(document.getElementById('nota1').value);
-    let nota2 = parseInt(document.getElementById('nota2').value);
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
     let mediaNotas = (nota1 + nota2) / 2
     if (mediaNotas >= 7) {
         let media1 = document.getElementById("textomedia");
